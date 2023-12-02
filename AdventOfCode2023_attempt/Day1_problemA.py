be935157b90655b5c82339d2463b8a2041aa37d2
hello <joshthoo@gmail.com>
2023-12-03 02:03:45 +0800

    day 1 problem A

--- AdventOfCode2023_attempt/Day1_problemA.py
+++ AdventOfCode2023_attempt/Day1_problemA.py
@@ -2,16 +2,30 @@
 day_number = 1
 problem_set = "A"
 
+from functools import reduce
 def main():
     input = read_file()
+    print(input)
+    no_digits = map(lambda input_line: 
+                            filter(lambda char: char >= "0" and char <= "9",
+                                   input_line),
+                    input)
+    
+    sum = 0
+    for item in no_digits:
+        arr = list(item)
+        if len(arr) != 0:
+            sum += int(arr[0]) * 10 + int(arr[-1])
+    print(''.join(no_digits))
+    
 
 def read_file():
     relative_directory = "personal-projects/AdventOfCode2023_attempt"
     file_lines = []
+    #looped instead of fully read with .readlines() to remove new line character at the back
     with open(f"{relative_directory}/Day{day_number}_Problem{problem_set}_Input.txt", "r") as file:
         for line in file:
-            #exclude new line character at the back
-            file_lines.append(line[:-2])
+            file_lines.append(line[:-1])
     return file_lines
 
 main()
--- AdventOfCode2023_attempt/testfunctions.py
+++
@@ -1,6 +0,0 @@
-from functools import reduce
-x = ["abcdef1234567890!@%#", "qww223"]
-
-y = map(lambda a: filter(lambda b: b >= "1" and b <= "9", a), x)
-sum = reduce(lambda x, acc: x + acc, [1,2,3,4,5,6,7,8,9])
-print(sum)
