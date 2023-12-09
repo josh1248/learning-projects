539074452848528b193a4fa9cd4aacb0ac9b16f4
josh1248 <joshthoo@gmail.com>
2023-12-09 17:42:06 +0800

    reduced needed int typecasting in function

--- AdventOfCode2023_attempt/Day9_ProblemAB.py
+++ AdventOfCode2023_attempt/Day9_ProblemAB.py
@@ -2,11 +2,10 @@
 day_number = 9
 def main():
     input = read_file()
-    sequences = [line.split() for line in input]
-
+    sequences = [[int(i) for i in line.split()] for line in input]
     def extrapolate(seq):
-        diffs = [int(seq[i + 1]) - int(seq[i]) for i in range(len(seq) - 1)]
-        return int(seq[-1]) + (extrapolate(diffs) if any(diffs) else 0)    
+        diffs = [seq[i + 1] - seq[i] for i in range(len(seq) - 1)]
+        return seq[-1] + (extrapolate(diffs) if any(diffs) else 0)    
     
     print(sum(map(extrapolate, sequences)))
     print(sum(map(extrapolate, [seq[::-1] for seq in sequences])))
